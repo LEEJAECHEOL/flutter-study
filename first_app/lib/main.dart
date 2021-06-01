73a5d0de86cf68e6cfff0d969b8da84f9eff9e7e
LEEJAECHEOL <jae_choel@naver.com>
2021-06-01 20:58:41 +0900

    목록 아이콘 추가 및 상호작용맛보기, 네비게이터사용해보기git add . end :)

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -9,7 +9,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title : "Startup Name Generator",
+        title: "Startup Name Generator",
+        theme: ThemeData(
+          primaryColor: Colors.white,
+        ),
         home: RandomWords()
     );
   }
@@ -23,40 +26,85 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title : Text("Startup Name Generator"),
+        title: Text("Startup Name Generator"),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
 
-  Widget _buildSuggestions(){
+  void _pushSaved() {
+    Navigator.of(context).push(
+        // ignore: missing_return
+        MaterialPageRoute(builder: (BuildContext context) {
+          final tiles = _saved.map(
+                  (WordPair wordPair) {
+                return ListTile(
+                  title: Text(wordPair.asPascalCase, style: _biggerFont,),
+                );
+              });
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Saved Suggestion"),
+            ),
+            body: ListView(children: divided,),
+          );
+        }
+        ),
+    );
+  }
+
+  Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
         // ignore: missing_return
-        itemBuilder:(BuildContext _context, int i){
-          if(i.isOdd){
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
             return Divider();
           }
           final int index = i ~/ 2;
-          if(index >= _suggestions.length){
+          if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
         }
     );
   }
-  Widget _buildRow(WordPair wordPair){
+
+  Widget _buildRow(WordPair wordPair) {
+    final alreadySaved = _saved.contains(wordPair);
     return ListTile(
       title: Text(
         wordPair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(wordPair);
+          } else {
+            _saved.add(wordPair);
+          }
+        });
+      },
     );
   }
 }
