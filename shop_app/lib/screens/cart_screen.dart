e9cf1182497764ed2d259341334a4f592bd6077b
LEEJAECHEOL <jae_choel@naver.com>
2021-06-30 21:46:05 +0900

    add orders

--- shop_app/lib/screens/cart_screen.dart
+++ shop_app/lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart' show Cart;
+import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/widgets/cart_item.dart' as ci;
 
 class CartScreen extends StatelessWidget {
@@ -39,7 +40,13 @@ class CartScreen extends StatelessWidget {
                   FlatButton(
                     child: Text("ORDER NOW"),
                     textColor: Theme.of(context).primaryColor,
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                          cart.items.values.toList(),
+                          cart.totalAmount
+                      );
+                      cart.clear();
+                    },
                   ),
                 ],
               ),
