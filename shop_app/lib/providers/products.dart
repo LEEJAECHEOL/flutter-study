101e8b0c29e772f75e24fc342b47eaff3f3e7fae
LEEJAECHEOL <jae_choel@naver.com>
2021-07-03 23:43:37 +0900

    add or edit form 추가

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -64,8 +64,31 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  void addProduct(){
-    // _items.add(value);
+  void addProduct(Product product){
+    final newProduct = Product(
+      title:product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+      id: DateTime.now().toString()
+    );
+    _items.add(newProduct);
+    notifyListeners();
+  }
+
+  void updateProduct(String id, Product newProduct){
+    final productIndex = _items.indexWhere((product) => product.id == id);
+    if(productIndex >= 0){
+      _items[productIndex] = newProduct;
+    }else{
+      print('...');
+    }
+    notifyListeners();
+
+  }
+
+  void deleteProducts(String id){
+    _items.removeWhere((product) => product.id == id);
     notifyListeners();
   }
 
--- shop_app/lib/screens/user_products_screen.dart
+++ shop_app/lib/screens/user_products_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/user_product_item.dart';
 
@@ -14,7 +15,11 @@ class UserProductsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Your Products"),
         actions: [
-          IconButton(icon: const Icon(Icons.add), onPressed: () {}),
+          IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routeName);
+              }),
         ],
       ),
       drawer: AppDrawer(),
@@ -25,8 +30,9 @@ class UserProductsScreen extends StatelessWidget {
           itemBuilder: (_, i) => Column(
             children: [
               UserProductItem(
+                productsData.items[i].id,
                 productsData.items[i].title,
-                productsData.items[i].imageUrl
+                productsData.items[i].imageUrl,
               ),
               Divider()
             ]
